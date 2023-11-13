3352f17a1ac44dc8241d144a258bb25d476a85ed
Meike <92315934+mjung2605@users.noreply.github.com>
2023-11-13 11:34:23 +0100

    Erstellen der Spring-Anwendung

---
+++ DJManager/src/main/kotlin/de/thkoeln/gm/djmanager/DjManagerApplication.kt
@@ -0,0 +1,11 @@
+package de.thkoeln.gm.djmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DjManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<DjManagerApplication>(*args)
+}
---
+++ DJManager/src/test/kotlin/de/thkoeln/gm/djmanager/DjManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.thkoeln.gm.djmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DjManagerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
