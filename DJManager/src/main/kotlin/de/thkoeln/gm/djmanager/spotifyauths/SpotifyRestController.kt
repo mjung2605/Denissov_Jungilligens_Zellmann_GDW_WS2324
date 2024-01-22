7a3fbb6d4cf7e33660d79c90d75de1b83467079d
Alex Zellmann <alex.zellmann@hotmail.de>
2024-01-22 21:17:30 +0100

    Use Case: Nach einem Song im Spotify-Web-Service suchen und Songs in die Partyplaylist aufnehmen/abspeichern

--- DJManager/src/main/kotlin/de/thkoeln/gm/djmanager/spotifyauths/SpotifyRestController.kt
+++ DJManager/src/main/kotlin/de/thkoeln/gm/djmanager/spotifyauths/SpotifyRestController.kt
@@ -1,5 +1,6 @@
 package de.thkoeln.gm.djmanager.spotifyauths
 
+import de.thkoeln.gm.djmanager.songs.SongsService
 import org.springframework.http.HttpEntity
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
@@ -7,21 +8,79 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
 import java.net.http.HttpHeaders
-
+import org.json.JSONObject
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.HttpClients
+import org.apache.http.util.EntityUtils
+import de.thkoeln.gm.djmanager.songs.Song
 @RestController
-class SpotifySearchController {
+class SpotifySearchController(private val songsService: SongsService) {
 
-    @GetMapping("/spotifyauths")
+    @GetMapping("/spotifyauth")
     fun searchSong(
-        @RequestParam("query") query: String,
+        @RequestParam("song") song: String,
+        @RequestParam("interpret") interpret: String,
         @RequestParam("accessToken") accessToken: String
     ): Any {
         val restTemplate = RestTemplate()
 
-        val uri = "https://api.spotify.com/v1/search?q=$query&type=track"
-
+        val uri = "https://api.spotify.com/v1/search?q=track:$song&artist:$interpret&type=track&limit=10"
         val responseEntity = restTemplate.getForEntity("$uri&access_token=$accessToken", Any::class.java)
-
         return responseEntity.body ?: "No results found"
     }
+
+
+    @GetMapping("/findAndSaveOneSong")
+    fun findAndSaveOneSong(
+        @RequestParam("song") song: String,
+        @RequestParam("interpret") interpret: String,
+        @RequestParam("accessToken") accessToken: String
+    ): Boolean {
+        val songs:List<String> = getSongsByTitleAndInterpret(song, interpret, accessToken)
+        if(songs.size==1){
+            saveSong(songs[0])
+            return true
+        }
+        else{
+            return false
+        }
+    }
+
+    @GetMapping("/findSongs")
+    fun findSongs(
+        @RequestParam("song") song: String,
+        @RequestParam("interpret") interpret: String,
+        @RequestParam("accessToken") accessToken: String
+    ): List<String> {
+        val songs:List<String> = getSongsByTitleAndInterpret(song, interpret, accessToken)
+        return songs
+    }
+
+    @GetMapping("/saveSong")
+    fun findSongs(
+        @RequestParam("titel") titel: String,
+    ) {
+        saveSong(titel)
+    }
+
+    fun saveSong(titel:String){
+       var song = Song()
+       song.name = titel
+       songsService.saveSong(song)
+   }
+
+    fun getSongsByTitleAndInterpret(title: String, interpret: String, accessToken: String): List<String> {
+        val client = HttpClients.createDefault()
+        val request = HttpGet("https://api.spotify.com/v1/search?q=track:$title&artist:$interpret&type=track")
+        request.addHeader("Authorization", "Bearer $accessToken")
+        val response = client.execute(request)
+        val data = JSONObject(EntityUtils.toString(response.entity))
+        val songs = mutableListOf<String>()
+        val items = data.getJSONObject("tracks").getJSONArray("items")
+        for (i in 0 until items.length()) {
+            songs.add(items.getJSONObject(i).getString("name"))
+        }
+        return songs
+    }
+
 }
